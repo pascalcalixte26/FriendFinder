114bd54f84460f83105c1771db520f583cbef2a3
Pascal <pascal.calixte26@gmail.com>
2019-06-16 01:07:57 -0400

    last try for friend finder

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -35,7 +35,7 @@ module.exports = function(app) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-    if (friendsData.length < 14) {
+    if (friendsData.length < 13) {
       friendsData.push(req.body);
       res.json(true);
     }
